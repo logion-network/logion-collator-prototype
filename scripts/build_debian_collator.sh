57907913ddffe2445cdd2cc10771360efa68e93f
Gérard Dethier <gerard@logion.network>
2022-06-08 16:01:12 +0200

    chore: rename Dockerfile.

--- scripts/build_debian_collator.sh
+++ scripts/build_debian_collator.sh
@@ -2,5 +2,5 @@
 
 BUILDER_IMAGE=logionnetwork/logion-collator-debian-builder:latest
 
-docker build . -t $BUILDER_IMAGE
+docker build . -f ./Dockerfile.build-debian11 -t $BUILDER_IMAGE
 docker run --rm -it -v $(pwd)/debian-bin:/target/logion-collator $BUILDER_IMAGE
